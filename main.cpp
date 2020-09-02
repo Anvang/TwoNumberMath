b2db4669ba9cde8545f44c6e74eba03898e33a26
repl.it user <replituser@example.com>
2020-09-02 02:18:24 +0000

    Program with some errors

--- main.cpp
+++ main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-  
+  //declare necessary variables...
+  int firstNumber
+  int secondNumber
+  int results
+
+  //request the user to enter two whole numbers...
+  cout<<Please enter a whole number: ;
+  cin>>firstNumber;
+  cout<<Please enter another: << secondNumber;
+
+  results = firstNumber + secondNumber;
+  cout<<firstNumber " plus " secondNumber " is " results <<endl;
   return 0;
 }
